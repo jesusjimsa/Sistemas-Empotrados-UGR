e5125ff45bd71443a62ed80d565cac2dda45d545
jesusjimsa <jesusjimsa@icloud.com>
2018-10-08 13:03:30 +0200

    Tabs

--- hello/hello.s
+++ hello/hello.s
@@ -7,65 +7,65 @@
 @ Constantes
 @
 
-        @ Registro de control de dirección del GPIO32-GPIO63
-        .set GPIO_PAD_DIR1,    0x80000004
+	@ Registro de control de dirección del GPIO32-GPIO63
+	.set GPIO_PAD_DIR1,    0x80000004
 
-        @ Registro de activación de bits del GPIO32-GPIO63
-        .set GPIO_DATA_SET1,   0x8000004c
+	@ Registro de activación de bits del GPIO32-GPIO63
+	.set GPIO_DATA_SET1,   0x8000004c
 
-        @ Registro de limpieza de bits del GPIO32-GPIO63
-        .set GPIO_DATA_RESET1, 0x80000054
+	@ Registro de limpieza de bits del GPIO32-GPIO63
+	.set GPIO_DATA_RESET1, 0x80000054
 
-        @ El led rojo está en el GPIO 44 (el bit 12 de los registros GPIO_X_1)
-        .set LED_RED_MASK,     (1 << (44-32))
+	@ El led rojo está en el GPIO 44 (el bit 12 de los registros GPIO_X_1)
+	.set LED_RED_MASK,     (1 << (44-32))
 
-        @ Retardo para el parpadeo
-        .set DELAY,            0x00080000
+	@ Retardo para el parpadeo
+	.set DELAY,            0x00080000
 
 @
 @ Punto de entrada
 @
 
-        .code 32
-        .text
-        .global _start
-        .type   _start, %function
+	.code 32
+	.text
+	.global _start
+	.type   _start, %function
 
 _start:
-        @ Configuramos el GPIO44 para que sea de salida
-        ldr     r4, =GPIO_PAD_DIR1
-        ldr     r5, =LED_RED_MASK
-        str     r5, [r4]
+	@ Configuramos el GPIO44 para que sea de salida
+	ldr     r4, =GPIO_PAD_DIR1
+	ldr     r5, =LED_RED_MASK
+	str     r5, [r4]
 
-        @ Direcciones de los registros GPIO_DATA_SET1 y GPIO_DATA_RESET1
-        ldr     r6, =GPIO_DATA_SET1
-        ldr     r7, =GPIO_DATA_RESET1
+	@ Direcciones de los registros GPIO_DATA_SET1 y GPIO_DATA_RESET1
+	ldr     r6, =GPIO_DATA_SET1
+	ldr     r7, =GPIO_DATA_RESET1
 
 loop:
-        @ Encendemos el led
-        str     r5, [r6]
+	@ Encendemos el led
+	str     r5, [r6]
 
-        @ Pausa corta
-        ldr     r0, =DELAY
-        bl      pause
+	@ Pausa corta
+	ldr     r0, =DELAY
+	bl      pause
 
-        @ Apagamos el led
-        str     r5, [r7]
+	@ Apagamos el led
+	str     r5, [r7]
 
-        @ Pausa corta
-        ldr     r0, =DELAY
-        bl      pause
+	@ Pausa corta
+	ldr     r0, =DELAY
+	bl      pause
 
-        @ Bucle infinito
-        b       loop
-        
+	@ Bucle infinito
+	b       loop
+	
 @
 @ Función que produce un retardo
 @ r0: iteraciones del retardo
 @
-        .type   pause, %function
+	.type   pause, %function
 pause:
-        subs    r0, r0, #1
-        bne     pause
-        mov     pc, lr
+	subs    r0, r0, #1
+	bne     pause
+	mov     pc, lr
 
