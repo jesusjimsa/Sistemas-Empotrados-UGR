e3afe2420d06504a970e48547c629f72a8183b63
jesusjimsa <jesusjimsa@icloud.com>
2018-11-08 16:47:44 +0100

    Curly brackets in the "right" place

--- app/hello.c
+++ app/hello.c
@@ -33,8 +33,7 @@ uint32_t const delay = 0x10000;
 /*
  * Inicialización de los pines de E/S
  */
-void gpio_init(void)
-{
+void gpio_init(void){
 	/* Configuramos el GPIO44 para que sea de salida */
 	*reg_gpio_pad_dir1 = led_red_mask;
 }
@@ -45,8 +44,7 @@ void gpio_init(void)
  * Enciende los leds indicados en la máscara
  * @param mask Máscara para seleccionar leds
  */
-void leds_on (uint32_t mask)
-{
+void leds_on (uint32_t mask){
 	/* Encendemos los leds indicados en la máscara */
 	*reg_gpio_data_set1 = mask;
 }
@@ -57,8 +55,7 @@ void leds_on (uint32_t mask)
  * Apaga los leds indicados en la máscara
  * @param mask Máscara para seleccionar leds
  */
-void leds_off (uint32_t mask)
-{
+void leds_off (uint32_t mask){
 	/* Apagamos los leds indicados en la máscara */
 	*reg_gpio_data_reset1 = mask;
 }
@@ -68,8 +65,7 @@ void leds_off (uint32_t mask)
 /*
  * Retardo para el parpedeo
  */
-void pause(void)
-{
+void pause(void){
         uint32_t i;
 	for (i=0 ; i<delay ; i++);
 }
@@ -84,22 +80,20 @@ uint32_t the_led;
 /*
  * Programa principal
  */
-int main ()
-{
+int main (){
 	gpio_init();
 
-        the_led = led_red_mask;
+    the_led = led_red_mask;
 
-	while (1)
-	{
+	while (1){
 		leds_on(the_led);
-                pause();
+        pause();
 
 		leds_off(the_led);
-                pause();
+        pause();
 	}
 
-        return 0;
+    return 0;
 }
 
 /*****************************************************************************/
