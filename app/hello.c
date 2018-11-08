bd3fcfdda13f8e680f305a6318bc7a21411420d7
jesusjimsa <jesusjimsa@icloud.com>
2018-11-08 17:51:10 +0100

    Test buttons

--- app/hello.c
+++ app/hello.c
@@ -17,11 +17,23 @@ volatile uint32_t * const reg_gpio_pad_dir1    = (uint32_t *) 0x80000004;
 /* Dirección del registro de activación de bits del GPIO32-GPIO63 */
 volatile uint32_t * const reg_gpio_data_set1   = (uint32_t *) 0x8000004c;
 
+/* Dirección del registro de control de botones del GPIO00-GPIO31 */
+volatile uint32_t * const reg_gpio_data0 = (uint32_t *) 0x80000008;
+
 /* Dirección del registro de limpieza de bits del GPIO32-GPIO63 */
 volatile uint32_t * const reg_gpio_data_reset1 = (uint32_t *) 0x80000054;
 
 /* El led rojo está en el GPIO 44 (el bit 12 de los registros GPIO_X_1) */
 uint32_t const led_red_mask = (1 << (44-32));
+
+/* El led verde está en el GPIO 45 */
+uint32_t const led_green_mask = (1 << (45-32));
+
+/* Switches */
+uint32_t const sw2_input_mask = (1 << 27);
+uint32_t const sw3_input_mask = (1 << 26);
+uint32_t const sw2_output_mask = (1 << 23);
+uint32_t const sw3_output_mask = (1 << 22);
 
 /*
  * Constantes relativas a la aplicacion
@@ -34,8 +46,10 @@ uint32_t const delay = 0x10000;
  * Inicialización de los pines de E/S
  */
 void gpio_init(void){
-	/* Configuramos el GPIO44 para que sea de salida */
-	*reg_gpio_pad_dir1 = led_red_mask;
+	/* Configuramos los GPIO44 y GPIO45 para que sean de salida */
+	*reg_gpio_pad_dir1 = (led_red_mask | led_green_mask);
+	/* Set SW pins to high */
+	*reg_gpio_data_set1 = (sw2_output_mask | sw3_output_mask);
 }
 
 /*****************************************************************************/
@@ -62,12 +76,25 @@ void leds_off (uint32_t mask){
 
 /*****************************************************************************/
 
+void test_buttons(void){
+	if(*reg_gpio_data0 == sw3_input_mask){
+		the_led = led_green_mask;
+	}
+	else{
+		if(*reg_gpio_data0 == sw2_input_mask){
+			the_led = led_red_mask;
+		}
+	}
+}
+
+/*****************************************************************************/
+
 /*
- * Retardo para el parpedeo
+ * Retardo para el parpadeo
  */
 void pause(void){
     uint32_t i;
-	for (i=0 ; i<delay ; i++);
+	for (i = 0; i < delay; i++);
 }
 
 /*****************************************************************************/
@@ -86,6 +113,7 @@ int main (){
     the_led = led_red_mask;
 
 	while (1){
+		test_buttons();
 		leds_on(the_led);
         pause();
 
