cad7652a21e1757e479070ea7668ae97c029a348
Jesús Jiménez Sánchez <jesusjimsa@icloud.com>
2018-11-20 23:15:21 +0100

    itc_regs_t struct and enable and disable interruptions

--- bsp/drivers/itc.c
+++ bsp/drivers/itc.c
@@ -10,9 +10,20 @@
 /**
  * Acceso estructurado a los registros de control del ITC del MC1322x
  */
-typedef struct
-{
-	/* ESTA ESTRUCTURA SE DEFINIRÁ EN LA PRÁCTICA 6 */
+typedef struct{
+	volatile uint32_t intcntl;
+	volatile uint32_t nimask;
+	volatile uint32_t intennum;
+	volatile uint32_t intidsnum;
+	volatile uint32_t intenable;
+	volatile uint32_t inttype;
+	const uint32_t RESERVED[4];
+	volatile const uint32_t nivector;
+	volatile const uint32_t fivector;
+	volatile const uint32_t intsrc;
+	volatile uint32_t intfrc;
+	volatile const uint32_t nipend;
+	volatile const uint32_t fipend;
 } itc_regs_t;
 
 static volatile itc_regs_t* const itc_regs = ITC_BASE;
@@ -30,8 +41,7 @@ static itc_handler_t itc_handlers[itc_src_max];
  * y habilita el arbitraje de interrupciones Normales y rápidas en el controlador
  * de interupciones.
  */
-inline void itc_init ()
-{
+inline void itc_init(){
 	/* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 6 */
 }
 
@@ -41,8 +51,7 @@ inline void itc_init ()
  * Deshabilita el envío de peticiones de interrupción a la CPU
  * Permite implementar regiones críticas en modo USER
  */
-inline void itc_disable_ints ()
-{
+inline void itc_disable_ints(){
 	/* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 6 */
 }
 
@@ -52,8 +61,7 @@ inline void itc_disable_ints ()
  * Vuelve a habilitar el envío de peticiones de interrupción a la CPU
  * Permite implementar regiones críticas en modo USER
  */
-inline void itc_restore_ints ()
-{
+inline void itc_restore_ints(){
 	/* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 6 */
 }
 
@@ -64,8 +72,7 @@ inline void itc_restore_ints ()
  * @param src		Identificador de la fuente
  * @param handler	Manejador
  */
-inline void itc_set_handler (itc_src_t src, itc_handler_t handler)
-{
+inline void itc_set_handler(itc_src_t src, itc_handler_t handler){
 	/* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 6 */
 }
 
@@ -76,8 +83,7 @@ inline void itc_set_handler (itc_src_t src, itc_handler_t handler)
  * @param src		Identificador de la fuente
  * @param priority	Tipo de prioridad
  */
-inline void itc_set_priority (itc_src_t src, itc_priority_t priority)
-{
+inline void itc_set_priority(itc_src_t src, itc_priority_t priority){
 	/* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 6 */
 }
 
@@ -87,9 +93,10 @@ inline void itc_set_priority (itc_src_t src, itc_priority_t priority)
  * Habilita las interrupciones de una determinda fuente
  * @param src		Identificador de la fuente
  */
-inline void itc_enable_interrupt (itc_src_t src)
-{
-	/* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 6 */
+inline void itc_enable_interrupt(itc_src_t src){
+	// Es el bit 000...001 desplazado a la izquierda 'src' veces,
+	// para habilitar las interrupciones correspondientes
+	itc_regs->intenable |= (uint32_t)(1 << src);
 }
 
 /*****************************************************************************/
@@ -98,9 +105,10 @@ inline void itc_enable_interrupt (itc_src_t src)
  * Deshabilita las interrupciones de una determinda fuente
  * @param src		Identificador de la fuente
  */
-inline void itc_disable_interrupt (itc_src_t src)
-{
-	/* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 6 */
+inline void itc_disable_interrupt(itc_src_t src){
+	// Hace el mismo desplazamiento que en la función anterior y AND para
+	// poner a 0 solo el bit correspondiente
+	itc_regs->intenable &= ~(uint32_t)(1 << src);
 }
 
 /*****************************************************************************/
@@ -109,8 +117,7 @@ inline void itc_disable_interrupt (itc_src_t src)
  * Fuerza una interrupción con propósitos de depuración
  * @param src		Identificador de la fuente
  */
-inline void itc_force_interrupt (itc_src_t src)
-{
+inline void itc_force_interrupt(itc_src_t src){
 	/* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 6 */
 }
 
@@ -120,8 +127,7 @@ inline void itc_force_interrupt (itc_src_t src)
  * Desfuerza una interrupción con propósitos de depuración
  * @param src		Identificador de la fuente
  */
-inline void itc_unforce_interrupt (itc_src_t src)
-{
+inline void itc_unforce_interrupt(itc_src_t src){
 	/* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 6 */
 }
 
@@ -133,8 +139,7 @@ inline void itc_unforce_interrupt (itc_src_t src)
  * anidadas, debe deshabilitar las IRQ de menor prioridad hasta que se haya
  * completado el servicio de la IRQ para evitar inversiones de prioridad
  */
-void itc_service_normal_interrupt ()
-{
+void itc_service_normal_interrupt(){
 	/* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 6 */
 }
 
@@ -143,8 +148,7 @@ void itc_service_normal_interrupt ()
 /**
  * Da servicio a la interrupción rápida pendiente de más prioridad
  */
-void itc_service_fast_interrupt ()
-{
+void itc_service_fast_interrupt(){
 	/* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 6 */
 }
 
