dc34e2bc0ffddd69856855c98e3fc1fc6de5e28a
Jesús Jiménez Sánchez <jesusjimsa@icloud.com>
2018-11-22 11:57:02 +0100

    Inicialización del controlador de interrupciones

--- bsp/drivers/itc.c
+++ bsp/drivers/itc.c
@@ -47,7 +47,23 @@ static volatile uint32_t intenable_status;
  * de interupciones.
  */
 inline void itc_init(){
-	/* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 6 */
+	uint32_t i;
+
+	for(i = 0; i < itc_src_max; i++){
+		itc_handlers[i] = (uint32_t) NULL;
+	}
+
+	// No provocar ninguna interrupción simulada al arrancar
+	itc_regs->intfrc = (uint32_t) 0;
+
+	// Deshabilitar todas las fuentes de interrupción al activar el controlador
+	itc_regs->intenable = (uint32_t) 0;
+	
+	// Activar arbitraje de interrupciones IRQ
+	itc_regs->intcntl = ~(uint32_t)(1 << 19);
+	
+	// Activar arbitraje de interrupciones FIQ
+	itc_regs->intcntl = ~(uint32_t)(1 << 20);
 }
 
 /*****************************************************************************/
@@ -133,7 +149,7 @@ inline void itc_disable_interrupt(itc_src_t src){
  * @param src		Identificador de la fuente
  */
 inline void itc_force_interrupt(itc_src_t src){
-	/* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 6 */
+	itc_regs->intfrc |= (uint32_t)(1 << src);	// Ponemos el bit indicado a 1
 }
 
 /*****************************************************************************/
@@ -143,7 +159,7 @@ inline void itc_force_interrupt(itc_src_t src){
  * @param src		Identificador de la fuente
  */
 inline void itc_unforce_interrupt(itc_src_t src){
-	/* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 6 */
+	itc_regs->intfrc &= ~(uint32_t)(1 << src);
 }
 
 /*****************************************************************************/
