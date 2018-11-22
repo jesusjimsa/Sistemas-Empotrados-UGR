b18f264297125debcf97f58040469495b792c6e6
Jesús Jiménez Sánchez <jesusjimsa@icloud.com>
2018-11-22 18:32:21 +0100

    Práctica 6

---
+++ Practica_6/app/hello.c
@@ -0,0 +1,183 @@
+/*****************************************************************************/
+/*                                                                           */
+/* Sistemas Empotrados                                                       */
+/* El "hola mundo" en la Redwire EconoTAG en C                               */
+/*                                                                           */
+/*****************************************************************************/
+
+#include <stdint.h>
+#include "system.h"
+
+/*
+ * Constantes relativas a la plataforma
+ */
+
+/* Dirección del registro de control de dirección del GPIO00-GPIO31 */
+volatile uint32_t * const reg_gpio_pad_dir0    = (uint32_t *) 0x80000000;
+
+/* Dirección del registro de control de dirección del GPIO32-GPIO63 */
+volatile uint32_t * const reg_gpio_pad_dir1    = (uint32_t *) 0x80000004;
+
+/* Dirección del registro de activación de bits del GPIO00-GPIO31 */
+volatile uint32_t * const reg_gpio_data_set0   = (uint32_t *) 0x80000048;
+
+/* Dirección del registro de activación de bits del GPIO32-GPIO63 */
+volatile uint32_t * const reg_gpio_data_set1   = (uint32_t *) 0x8000004c;
+
+/* Dirección del registro de control de botones del GPIO00-GPIO31 */
+volatile uint32_t * const reg_gpio_data0 = (uint32_t *) 0x80000008;
+
+/* Dirección del registro de limpieza de bits del GPIO32-GPIO63 */
+volatile uint32_t * const reg_gpio_data_reset1 = (uint32_t *) 0x80000054;
+
+/* El led rojo está en el GPIO 44 (el bit 12 de los registros GPIO_X_1) */
+uint32_t const led_red_mask = (1 << (44-32));
+
+/* El led verde está en el GPIO 45 */
+uint32_t const led_green_mask = (1 << (45-32));
+
+/* KBI0 está en el GPIO 22 */
+uint32_t const kbi0_mask      = (1 << 22);
+
+/* kbi1 está en el gpio 23 */
+uint32_t const kbi1_mask      = (1 << 23);
+
+/* kbi4 está en el gpio 26 */
+uint32_t const kbi4_mask      = (1 << 26);
+
+/* kbi5 está en el gpio 27 */
+uint32_t const kbi5_mask      = (1 << 27);
+
+/*
+ * Constantes relativas a la aplicacion
+ */
+uint32_t const delay = 0x10000;
+ 
+/*****************************************************************************/
+
+/*
+ * Inicialización de los pines de E/S
+ */
+void gpio_init(void){
+	/* Configuramos los GPIO44 y GPIO45 para que sean de salida */
+	*reg_gpio_pad_dir1 = (led_red_mask | led_green_mask);
+
+	/* Configuramos los KBI0 y KBI1 para que sean de salida */
+	/* y KBI4 y KBI5 para que sean de entrada */
+	*reg_gpio_pad_dir0 = kbi0_mask | kbi1_mask;
+
+	/* Fijamos un 1 en KBI0 y KBI1 */
+	*reg_gpio_data_set0 = kbi0_mask | kbi1_mask;
+}
+
+/*****************************************************************************/
+
+/*
+ * Enciende los leds indicados en la máscara
+ * @param mask Máscara para seleccionar leds
+ */
+void leds_on (uint32_t mask){
+	/* Encendemos los leds indicados en la máscara */
+	*reg_gpio_data_set1 = mask;
+}
+
+/*****************************************************************************/
+
+/*
+ * Apaga los leds indicados en la máscara
+ * @param mask Máscara para seleccionar leds
+ */
+void leds_off (uint32_t mask){
+	/* Apagamos los leds indicados en la máscara */
+	*reg_gpio_data_reset1 = mask;
+}
+
+/*****************************************************************************/
+
+/*
+ * Selecciona el led que se debe parpadear en función del estado de los botones
+ * @param current_led  Máscara del led actualmente selecionado
+ * @return             Máscara del led que se seguirá parpadeando
+ */
+uint32_t test_buttons(uint32_t current_led){
+	uint32_t the_led, data0;
+
+	data0 = *reg_gpio_data0;
+	the_led = current_led;
+
+	if (data0 & kbi4_mask){
+		the_led = led_green_mask;
+	}
+	else{
+		if (data0 & kbi5_mask){
+			the_led = led_red_mask;
+		}
+	}
+
+	return the_led;
+}
+
+/*****************************************************************************/
+
+/*
+ * Retardo para el parpadeo
+ */
+void pause(void){
+	uint32_t i;
+	for (i = 0; i < delay; i++);
+}
+
+/*****************************************************************************/
+
+/*
+ * Manejador de instrucciones no definidas
+ */
+__attribute__ ((interrupt("UNDEF")))
+void undef_handler(void){
+    *reg_gpio_data_set1 = led_green_mask;
+}
+
+/*****************************************************************************/
+
+/*
+ * Manejador de interrupciones ASM 
+ */
+void asm_handler(void){
+	itc_unforce_interrupt(itc_src_asm);
+    *reg_gpio_data_set1 = led_green_mask;
+}
+
+/*****************************************************************************/
+
+/*
+ * Programa principal
+ */
+int main (){
+	uint32_t the_led;	// Máscara del led que se hará parpadear
+
+	gpio_init();
+	
+	itc_set_handler(itc_src_asm, asm_handler);
+	excep_set_handler(excep_undef, undef_handler);
+
+	itc_enable_interrupt(itc_src_asm);
+    itc_force_interrupt(itc_src_asm);
+
+	the_led = led_red_mask;
+
+	asm(".word 0x26889912\n");
+	
+	while (1){
+		the_led = test_buttons(the_led);
+		leds_on(the_led);
+		pause();
+
+		leds_off(the_led);
+		pause();
+	}
+
+	return 0;
+}
+
+/*****************************************************************************/
+
---
+++ Practica_6/bsp/drivers/itc.c
@@ -0,0 +1,197 @@
+/*
+ * Sistemas operativos empotrados
+ * Driver para el controlador de interrupciones del MC1322x
+ */
+
+#include "system.h"
+
+/*****************************************************************************/
+
+/**
+ * Acceso estructurado a los registros de control del ITC del MC1322x
+ */
+typedef struct{
+	volatile uint32_t intcntl;
+	volatile uint32_t nimask;
+	volatile uint32_t intennum;
+	volatile uint32_t intidsnum;
+	volatile uint32_t intenable;
+	volatile uint32_t inttype;
+	const uint32_t RESERVED[4];
+	volatile const uint32_t nivector;
+	volatile const uint32_t fivector;
+	volatile const uint32_t intsrc;
+	volatile uint32_t intfrc;
+	volatile const uint32_t nipend;
+	volatile const uint32_t fipend;
+} itc_regs_t;
+
+static volatile itc_regs_t* const itc_regs = ITC_BASE;
+
+/**
+ * Tabla de manejadores de interrupción.
+ */
+static itc_handler_t itc_handlers[itc_src_max];
+
+/**
+ *	Variable global para guardar intenable
+ */
+static volatile uint32_t intenable_status;
+
+/*****************************************************************************/
+
+/**
+ * Inicializa el controlador de interrupciones.
+ * Deshabilita los bits I y F de la CPU, inicializa la tabla de manejadores a NULL,
+ * y habilita el arbitraje de interrupciones Normales y rápidas en el controlador
+ * de interupciones.
+ */
+inline void itc_init(){
+	uint32_t i;
+
+	for(i = 0; i < itc_src_max; i++){
+		itc_handlers[i] = (uint32_t) NULL;
+	}
+
+	// No provocar ninguna interrupción simulada al arrancar
+	itc_regs->intfrc = (uint32_t) 0;
+
+	// Deshabilitar todas las fuentes de interrupción al activar el controlador
+	itc_regs->intenable = (uint32_t) 0;
+	
+	// Activar arbitraje de interrupciones IRQ
+	itc_regs->intcntl = ~(uint32_t)(1 << 19);
+	
+	// Activar arbitraje de interrupciones FIQ
+	itc_regs->intcntl = ~(uint32_t)(1 << 20);
+}
+
+/*****************************************************************************/
+
+/**
+ * Deshabilita el envío de peticiones de interrupción a la CPU
+ * Permite implementar regiones críticas en modo USER
+ */
+inline void itc_disable_ints(){
+	intenable_status = itc_regs->intenable;
+
+	itc_regs->intenable = (uint32_t) 0;
+}
+
+/*****************************************************************************/
+
+/**
+ * Vuelve a habilitar el envío de peticiones de interrupción a la CPU
+ * Permite implementar regiones críticas en modo USER
+ */
+inline void itc_restore_ints(){
+	itc_regs->intenable = intenable_status;
+}
+
+/*****************************************************************************/
+
+/**
+ * Asigna un manejador de interrupción
+ * @param src		Identificador de la fuente
+ * @param handler	Manejador
+ */
+inline void itc_set_handler(itc_src_t src, itc_handler_t handler){
+	itc_handlers[src] = handler;
+}
+
+/*****************************************************************************/
+
+/**
+ * Asigna una prioridad (normal o fast) a una fuente de interrupción
+ * @param src		Identificador de la fuente
+ * @param priority	Tipo de prioridad
+ */
+inline void itc_set_priority(itc_src_t src, itc_priority_t priority){
+	if(priority){
+		// Las interrupciones FIQ son las de alta prioridad, por lo que
+		// será el único bit activo, así que se usa = en lugar de hacer
+		// OR o AND
+		itc_regs->inttype = (uint32_t)(1 << src);
+	}
+	else{
+		itc_regs->inttype &= ~(uint32_t)(1 << src);
+	}
+}
+
+/*****************************************************************************/
+
+/**
+ * Habilita las interrupciones de una determinda fuente
+ * @param src		Identificador de la fuente
+ */
+inline void itc_enable_interrupt(itc_src_t src){
+	// Es el bit 000...001 desplazado a la izquierda 'src' veces,
+	// para habilitar las interrupciones correspondientes
+	itc_regs->intenable |= (uint32_t)(1 << src);
+}
+
+/*****************************************************************************/
+
+/**
+ * Deshabilita las interrupciones de una determinda fuente
+ * @param src		Identificador de la fuente
+ */
+inline void itc_disable_interrupt(itc_src_t src){
+	// Hace el mismo desplazamiento que en la función anterior y AND para
+	// poner a 0 solo el bit correspondiente
+	itc_regs->intenable &= ~(uint32_t)(1 << src);
+}
+
+/*****************************************************************************/
+
+/**
+ * Fuerza una interrupción con propósitos de depuración
+ * @param src		Identificador de la fuente
+ */
+inline void itc_force_interrupt(itc_src_t src){
+	itc_regs->intfrc |= (uint32_t)(1 << src);	// Ponemos el bit indicado a 1
+}
+
+/*****************************************************************************/
+
+/**
+ * Desfuerza una interrupción con propósitos de depuración
+ * @param src		Identificador de la fuente
+ */
+inline void itc_unforce_interrupt(itc_src_t src){
+	itc_regs->intfrc &= ~(uint32_t)(1 << src);
+}
+
+/*****************************************************************************/
+
+/**
+ * Da servicio a la interrupción normal pendiente de más prioridad.
+ * En el caso de usar un manejador de excepciones IRQ que permita interrupciones
+ * anidadas, debe deshabilitar las IRQ de menor prioridad hasta que se haya
+ * completado el servicio de la IRQ para evitar inversiones de prioridad
+ */
+void itc_service_normal_interrupt(){
+	// Obtener el numero de interrupción más prioritaria
+    uint8_t priority = itc_regs->nivector;
+
+    // Deshabilitar las interrupciones menos prioritarias
+    itc_regs->nimask = priority;
+    
+	// Llamar al manejador de la interrupcion más prioritaria
+    itc_handlers[priority]();
+    
+	// Al retornar, rehabilitar todas las interrupciones
+    itc_regs->nimask = 0x31;
+}
+
+/*****************************************************************************/
+
+/**
+ * Da servicio a la interrupción rápida pendiente de más prioridad
+ */
+void itc_service_fast_interrupt(){
+	// Obtener el indice del manejador de la fiq y llamar a la rutina
+    itc_handlers[itc_regs->fivector]();
+}
+
+/*****************************************************************************/
