12825bad68906a008652ac8223933e9b82c1224e
Jesús Jiménez Sánchez <jesusjimsa@icloud.com>
2018-11-28 17:44:25 +0100

    Práctica 7

---
+++ Practica_7/app/hello.c
@@ -0,0 +1,161 @@
+/*****************************************************************************/
+/*                                                                           */
+/* Sistemas Empotrados                                                       */
+/* El "hola mundo" en la Redwire EconoTAG en C                               */
+/*                                                                           */
+/*****************************************************************************/
+
+#include <stdint.h>
+#include "system.h"
+
+/*
+ * Constantes relativas a la plataforma
+ */
+
+/* El led rojo está en el GPIO 44 (el bit 12 de los registros GPIO_X_1) */
+#define RED_LED gpio_pin_44
+
+/* El led verde está en el GPIO 45 */
+#define GREEN_LED gpio_pin_45
+
+/* KBI0 está en el GPIO 22 */
+#define KBI0 gpio_pin_22
+
+/* kbi1 está en el gpio 23 */
+#define KBI1 gpio_pin_23
+
+/* kbi4 está en el gpio 26 */
+#define KBI4 gpio_pin_26
+
+/* kbi5 está en el gpio 27 */
+#define KBI5 gpio_pin_27
+
+/*
+ * Constantes relativas a la aplicacion
+ */
+uint32_t const delay = 0x10000;
+ 
+/*****************************************************************************/
+
+/*
+ * Inicialización de los pines de E/S
+ */
+void gpio_init(void){
+	/* Configuramos los GPIO44 y GPIO45 para que sean de salida */
+	gpio_set_port_dir_output(gpio_port_1, 1 << (RED_LED - 32) | 1 << (GREEN_LED - 32));
+
+	//Configuramos los pines de los switches
+	gpio_set_port_dir_output(gpio_port_0, 1 << KBI0 | 1 << KBI1);
+	gpio_set_port_dir_input(gpio_port_0, 1 << KBI4 | 1 << KBI5);
+
+	//Ponemos un 1 en KBI0 y KBI1 para leer la pulsacion de los switches
+	gpio_set_port(gpio_port_0, 1 << KBI0 | 1 << KBI1);
+}
+
+/*****************************************************************************/
+
+/*
+ * Enciende el led indicado en el pin
+ * @param pin Máscara para seleccionar led
+ */
+void leds_on(uint32_t pin){
+	gpio_set_pin(pin);
+}
+
+/*****************************************************************************/
+
+/*
+ * Apaga el led indicado en el pin
+ * @param pin Máscara para seleccionar led
+ */
+void leds_off (uint32_t pin){
+	gpio_clear_pin(pin);
+}
+
+/*****************************************************************************/
+
+/*
+ * Selecciona el led que se debe parpadear en función del estado de los botones
+ * @param current_led  Máscara del led actualmente selecionado
+ * @return             Máscara del led que se seguirá parpadeando
+ */
+uint32_t test_buttons(uint32_t current_led){
+	uint32_t the_led, data0;
+
+	gpio_get_port(gpio_port_0, &data0);
+
+	the_led = current_led;
+
+	if (data0 & (1 << KBI4)){
+		the_led = GREEN_LED;
+	}
+	else{
+		if (data0 & (1 << KBI5)){
+			the_led = RED_LED;
+		}
+	}
+
+	return the_led;
+}
+
+/*****************************************************************************/
+
+/*
+ * Retardo para el parpadeo
+ */
+void pause(void){
+	uint32_t i;
+	for (i = 0; i < delay; i++);
+}
+
+/*****************************************************************************/
+
+/*
+ * Manejador de instrucciones no definidas
+ */
+__attribute__ ((interrupt("UNDEF")))
+void undef_handler(void){
+    leds_on(GREEN_LED);
+}
+
+/*****************************************************************************/
+
+/*
+ * Manejador de interrupciones ASM 
+ */
+void asm_handler(void){
+	itc_unforce_interrupt(itc_src_asm);
+    leds_on(GREEN_LED);
+}
+
+/*****************************************************************************/
+
+/*
+ * Programa principal
+ */
+int main (){
+	uint32_t the_led;	// Máscara del led que se hará parpadear
+
+	gpio_init();
+	
+	itc_set_handler(itc_src_asm, asm_handler);
+	excep_set_handler(excep_undef, undef_handler);
+
+	the_led = RED_LED;
+
+	gpio_set_pin(the_led);
+	
+	while (1){
+		the_led = test_buttons(the_led);
+		leds_on(the_led);
+		pause();
+
+		leds_off(the_led);
+		pause();
+	}
+
+	return 0;
+}
+
+/*****************************************************************************/
+
---
+++ Practica_7/bsp/drivers/gpio.c
@@ -0,0 +1,267 @@
+/*
+ * Sistemas operativos empotrados
+ * Driver para el GPIO del MC1322x
+ */
+
+#include "system.h"
+
+/*****************************************************************************/
+
+/**
+ * Acceso estructurado a los registros de control del gpio del MC1322x
+ */
+typedef struct{
+	uint32_t PAD_DIR[2];		// GPIO Pad Direction
+	uint32_t DATA[2];			// GPIO Data
+	uint32_t PAD_PU_EN[2];		// GPIO Pad Pull-up Enable
+	uint32_t FUNC_SEL[4];		// GPIO Function Select
+	uint32_t DATA_SEL[2];		// GPIO Data Select
+	uint32_t PAD_PU_SEL[2];		// GPIO Pad Pull-up Select
+	uint32_t PAD_HYST_EN[2];	// GPIO Pad Hysteresis Enable
+	uint32_t PAD_KEEP[2];		// GPIO Pad Keeper Enable
+	uint32_t DATA_SET[2];		// GPIO Data Set
+	uint32_t DATA_RESET[2];		// GPIO Data Reset
+	uint32_t PAD_DIR_SET[2];	// GPIO Pad Direction Set
+	uint32_t PAD_DIR_RESET[2];	// GPIO Pad Direction Reset
+} gpio_regs_t;
+
+static volatile gpio_regs_t* const gpio_regs = GPIO_BASE;
+
+/*****************************************************************************/
+
+/**
+ * Fija la dirección de los pines seleccionados en la máscara como de entrada
+ *
+ * @param 	port 	Puerto
+ * @param 	mask 	Máscara para seleccionar los pines
+ * @return	gpio_no_error si los parámetros de entrada son corectos o
+ *			gpio_invalid_parameter en otro caso
+ */
+inline gpio_err_t gpio_set_port_dir_input(gpio_port_t port, uint32_t mask){
+	if(port >= gpio_port_max){
+		return gpio_invalid_parameter;
+	}
+
+	gpio_regs->PAD_DIR_RESET[port] = mask;
+
+	return gpio_no_error;
+}
+
+/*****************************************************************************/
+
+/**
+ * Fija la dirección de los pines seleccionados en la máscara como de salida
+ *
+ * @param	port 	Puerto
+ * @param	mask 	Máscara para seleccionar los pines
+ * @return	gpio_no_error si los parámetros de entrada son corectos o
+ *			gpio_invalid_parameter en otro caso
+ */
+inline gpio_err_t gpio_set_port_dir_output(gpio_port_t port, uint32_t mask){
+	if(port >= gpio_port_max){
+		return gpio_invalid_parameter;
+	}
+	
+	gpio_regs->PAD_DIR_SET[port] = mask;
+
+	return gpio_no_error;
+}
+
+/*****************************************************************************/
+
+/**
+ * Fija la dirección del pin indicado como de entrada
+ *
+ * @param	pin 	Número de pin
+ * @return	gpio_no_error si los parámetros de entrada son corectos o
+ *			gpio_invalid_parameter en otro caso
+ */
+inline gpio_err_t gpio_set_pin_dir_input(gpio_pin_t pin){
+	if(pin >= gpio_pin_max){
+		return gpio_invalid_parameter;
+	}
+
+	gpio_regs->PAD_DIR_RESET[(pin >> 5) & 1] = 1 << (pin & 0x1f);
+
+	return gpio_no_error;
+}
+
+/*****************************************************************************/
+
+/**
+ * Fija la dirección del pin indicado como de salida
+ *
+ * @param	pin 	Número de pin
+ * @return	gpio_no_error si los parámetros de entrada son corectos o
+ *			gpio_invalid_parameter en otro caso
+ */
+inline gpio_err_t gpio_set_pin_dir_output(gpio_pin_t pin){
+	if(pin >= gpio_pin_max){
+		return gpio_invalid_parameter;
+	}
+
+	gpio_regs->PAD_DIR_SET[(pin >> 5) & 1] = 1 << (pin & 0x1f);
+
+	return gpio_no_error;
+}
+
+/*****************************************************************************/
+
+/**
+ * Escribe unos en los pines seleccionados en la máscara
+ *
+ * @param	port 	Puerto
+ * @param	mask 	Máscara para seleccionar los pines
+ * @return	gpio_no_error si los parámetros de entrada son corectos o
+ *			gpio_invalid_parameter en otro caso
+ */
+inline gpio_err_t gpio_set_port(gpio_port_t port, uint32_t mask){
+	if(port >= gpio_port_max){
+		return gpio_invalid_parameter;
+	}
+	
+	gpio_regs->DATA_SET[port] = mask;
+
+	return gpio_no_error;
+}
+
+/*****************************************************************************/
+
+/**
+ * Escribe ceros en los pines seleccionados en la máscara
+ *
+ * @param	port 	Puerto
+ * @param	mask 	Máscara para seleccionar los pines
+ * @return	gpio_no_error si los parámetros de entrada son corectos o
+ *			gpio_invalid_parameter en otro caso
+ */
+inline gpio_err_t gpio_clear_port(gpio_port_t port, uint32_t mask){
+	if(port >= gpio_port_max){
+		return gpio_invalid_parameter;
+	}
+	
+	gpio_regs->DATA_RESET[port] = mask;
+
+	return gpio_no_error;
+}
+
+/*****************************************************************************/
+
+/**
+ * Escribe un uno en el pin indicado
+ *
+ * @param	pin 	Número de pin
+ * @return	gpio_no_error si los parámetros de entrada son corectos o
+ *			gpio_invalid_parameter en otro caso
+ */
+inline gpio_err_t gpio_set_pin(gpio_pin_t pin){
+	if(pin >= gpio_pin_max){
+		return gpio_invalid_parameter;
+	}
+
+	gpio_regs->DATA_SET[(pin >> 5) & 1] = 1 << (pin & 0x1f);
+
+	return gpio_no_error;
+}
+
+/*****************************************************************************/
+
+/**
+ * Escribe un cero en el pin indicado
+ *
+ * @param	pin 	Número de pin
+ * @return	gpio_no_error si los parámetros de entrada son corectos o
+ *			gpio_invalid_parameter en otro caso
+ */
+inline gpio_err_t gpio_clear_pin(gpio_pin_t pin){
+	if(pin >= gpio_pin_max){
+		return gpio_invalid_parameter;
+	}
+
+	gpio_regs->DATA_RESET[(pin >> 5) & 1] = 1 << (pin & 0x1f);
+
+	return gpio_no_error;
+}
+
+/*****************************************************************************/
+
+/**
+ * Lee el valor de los pines de un puerto
+ *
+ * @param	port	  Puerto
+ * @param	port_data Valor de los pines del puerto
+ * @return	gpio_no_error si los parámetros de entrada son corectos o
+ *			  gpio_invalid_parameter en otro caso
+ */
+inline gpio_err_t gpio_get_port(gpio_port_t port, uint32_t *port_data){
+	if(port >= gpio_port_max){
+		return gpio_invalid_parameter;
+	}
+	
+	*port_data = gpio_regs->DATA[port];
+
+	return gpio_no_error;
+}
+
+/*****************************************************************************/
+
+/**
+ * Lee el valor del pin indicado
+ *
+ * @param	pin	  Número de pin
+ * @param       pin_data  Cero si el pin está a cero, distinto de cero en otro caso
+ * @return	gpio_no_error si los parámetros de entrada son corectos o
+ *			  gpio_invalid_parameter en otro caso
+ */
+inline gpio_err_t gpio_get_pin(gpio_pin_t pin, uint32_t *pin_data){
+	if(pin >= gpio_pin_max){
+		return gpio_invalid_parameter;
+	}
+	
+	pin_data = gpio_regs->DATA[pin];
+
+	return gpio_no_error;
+}
+
+/*****************************************************************************/
+
+/**
+ * Asigna un modo de funcionamiento a los pines seleccionados por la máscara
+ *
+ * @param   port    Puerto
+ * @param   func    Modo de funcionamiento
+ * @param	mask	Máscara para seleccionar los pines
+ * @return	gpio_no_error si los parámetros de entrada son corectos o
+ *			gpio_invalid_parameter en otro caso
+ */
+inline gpio_err_t gpio_set_port_func(gpio_port_t port, gpio_func_t func, uint32_t mask){
+	if(port >= gpio_port_max || func >= gpio_func_max){
+		return gpio_invalid_parameter;
+	}
+	
+	gpio_regs->FUNC_SEL[func] = mask;
+
+	return gpio_no_error;
+}
+
+/*****************************************************************************/
+
+/**
+ *  Asigna un modo de funcionamiento al pin seleccionado
+ *
+ * @param	pin 	Pin
+ * @param	func	Modo de funcionamiento
+ * @return	gpio_no_error si los parámetros de entrada son corectos o
+ *			gpio_invalid_parameter en otro caso
+ */
+inline gpio_err_t gpio_set_pin_func(gpio_pin_t pin, gpio_func_t func){
+	if(pin >= gpio_pin_max || func >= gpio_func_max){
+		return gpio_invalid_parameter;
+	}
+	
+	gpio_regs->FUNC_SEL[func] = 1 << (pin & 0x1f);
+
+	return gpio_no_error;
+}
+
+/*****************************************************************************/
